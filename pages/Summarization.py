f4747cf9c673d93c41643d84cf6b1c6f8ce5a4d7
Daydreammaker <sichenana98@gmail.com>
2024-03-15 10:58:27 +0100

    Make the summerization part workable

--- pages/Summarization.py
+++ pages/Summarization.py
@@ -11,52 +11,61 @@ from PIL import Image
 from datetime import datetime, timedelta
 import numpy as np
 import os
+from utils import (
+    doc_loader, summary_prompt_creator, doc_to_final_summary,
+)
+from my_prompts import file_map, file_combine, youtube_map, youtube_combine
+from streamlit_app_utils import check_gpt_4, check_key_validity, create_temp_file, create_chat_model, \
+    token_limit, token_minimum
+from utils import transcript_loader
 
 header1, header2, header3 = st.columns([1,12,1])
 body1, body2, body3 =st.columns([1,12,1])
 footer1, footer2, footer3 =st.columns([1,12,1])
 
-with header2: 
-    st.title("Summarization")
+def validate_doc_size(doc):
+    """
+    Validates the size of the document
 
-with body2:
-    st.header("Overview")
-    st.markdown("here it ould be nice to have some sort of explanaiton of the LL problem that is faced by a large number of people ")
-    st.image('https://github.com/engrobelf/low_literacy/blob/francois/picture/LL_pic.png?raw=True')
+    :param doc: doc to validate
 
-    st.header('Explanation experiment')
-    st.markdown('''To be modified: In this experiment we will show you four different profiles of passengers. 
-    Using Machine Learning (ML) we will show a prediction whether they would have survived the disaster. 
-    This prediction is accompanied by each time a different type of explanation.''')
-    st.markdown("After seeing four profiles, you will be asked to evaluate the explanation you have just seen.")
-    
-    st.subheader('Model')
-    st.markdown(''' The same ML model is used to generate the predictions of who survived and who did not. 
-                This model is used to generate all of the four types of explanations that you will see during the experiment. 
-                ''')
-    
+    :return: True if the doc is valid, False otherwise
+    """
+    if not token_limit(doc, 800000):
+        st.warning('File or transcript too big!')
+        return False
 
-input_method = st.radio("Select input method", ('Upload a document', 'another potential feature '))
-
-if input_method == 'Upload a document':
-    uploaded_file = st.file_uploader("Upload a document to summarize, 10k to 100k tokens works best!", type=['txt', 'pdf', 'png', 'jpeg'])
-    
-api_key = st.text_input("Enter API key here, or contact the author if you don't have one.")
-st.markdown('[Author email](mailto:f.m.g.leborgne@tue.nl)')
-use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - progress bar will appear to get stuck as GPT-4 is slow)", value=True)
-st.sidebar.markdown('# Made by: [François and Sichen ](https://github.com/engrobelf)')
-st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/engrobelf/low_literacy.git)') 
-st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
-                    This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
+    if not token_minimum(doc, 2000):
+        st.warning('File or transcript too small!')
+        return False
+    return True
 
 
-if st.button('Summarize (click once and wait)'):
-    if input_method == 'Upload a document':
-        process_summarize_button(uploaded_file, api_key, use_gpt_4, find_clusters)
+def validate_input(file_or_transcript, api_key, use_gpt_4):
+    """
+    Validates the user input, and displays warnings if the input is invalid
 
-    else:
-        doc = transcript_loader(youtube_url)
-        process_summarize_button(doc, api_key, use_gpt_4, find_clusters, file=False)
+    :param file_or_transcript: The file uploaded by the user or the YouTube URL entered by the user
+
+    :param api_key: The API key entered by the user
+
+    :param use_gpt_4: Whether the user wants to use GPT-4
+
+    :return: True if the input is valid, False otherwise
+    """
+    if file_or_transcript == None:
+        st.warning("Please upload a file or enter a YouTube URL.")
+        return False
+
+    if not check_key_validity(api_key):
+        st.warning('Key not valid or API is down.')
+        return False
+
+    if use_gpt_4 and not check_gpt_4(api_key):
+        st.warning('Key not valid for GPT-4.')
+        return False
+
+    return True
 
 
 def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_clusters, file=True):
@@ -104,6 +113,54 @@ def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_cluste
         st.markdown(summary, unsafe_allow_html=True)
         if file:
             os.unlink(temp_file_path)
+
+with header2: 
+    st.title("Summarization")
+
+with body2:
+    st.header("Overview")
+    st.markdown("here it ould be nice to have some sort of explanaiton of the LL problem that is faced by a large number of people ")
+    st.image('https://github.com/engrobelf/low_literacy/blob/francois/picture/LL_pic.png?raw=True')
+
+    st.header('Explanation experiment')
+    st.markdown('''To be modified: In this experiment we will show you four different profiles of passengers. 
+    Using Machine Learning (ML) we will show a prediction whether they would have survived the disaster. 
+    This prediction is accompanied by each time a different type of explanation.''')
+    st.markdown("After seeing four profiles, you will be asked to evaluate the explanation you have just seen.")
+    
+    st.subheader('Model')
+    st.markdown(''' The same ML model is used to generate the predictions of who survived and who did not. 
+                This model is used to generate all of the four types of explanations that you will see during the experiment. 
+                ''')
+    
+if 'uploaded_file' not in st.session_state:
+    st.session_state['uploaded_file'] = None
+    
+# input_method = st.radio("Select input method", ('Upload a document', 'another potential feature '))
+
+# if input_method == 'Upload a document':
+uploaded_file = st.file_uploader("Upload a document to summarize, 10k to 100k tokens works best!", type=['txt', 'pdf', 'png', 'jpeg'])
+    
+if uploaded_file is not None:
+    st.session_state['uploaded_file'] = uploaded_file
+
+api_key = st.text_input("Enter API key here, or contact the author if you don't have one.")
+st.markdown('[Author email](mailto:f.m.g.leborgne@tue.nl)')
+use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - progress bar will appear to get stuck as GPT-4 is slow)", value=True)
+find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
+st.sidebar.markdown('# Made by: [François and Sichen ](https://github.com/engrobelf)')
+st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/engrobelf/low_literacy.git)') 
+st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
+                    This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
+
+
+if st.button('Summarize (click once and wait)'):
+    if st.session_state ['uploaded_file'] is not None:
+            process_summarize_button(st.session_state['uploaded_file'], api_key, use_gpt_4, find_clusters)
+    else:
+        st.warning('plese uplaod your file')
+
+
 
 
 def validate_doc_size(doc):
