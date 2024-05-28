7fbaf3f12888802247c8c3d91ac3e2c575159004
engrobelf <128714297+engrobelf@users.noreply.github.com>
2024-05-28 14:18:42 +0200

    Update qualitative.py

--- pages/qualitative.py
+++ pages/qualitative.py
@@ -22,79 +22,15 @@ def record_page_duration_and_send():
         })
 
 
-def record_why1():
+def record_why(participant_id, why, question_id):
     st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_1': why[:900],
+        'participant_ID': participant_id,
+        f'why_{question_id}': why[:900],
     })
-    if len(why)>900:
+    if len(why) > 900:
         st.session_state.oocsi.send('LL_qualitative', {
-            'participant_ID': st.session_state.participantID,
-            'why_1_bis': why[900:1800],
-        })
-
-
-def record_why2():
-    st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_2': why_2[:900],
-    })
-    if len(why_2)>900:
-        st.session_state.oocsi.send('LL_qualitative', {
-            'participant_ID': st.session_state.participantID,
-            'why_2_bis': why_2[900:1800],
-        })
-
-
-def record_why2_bis():
-    st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_2_bis': why_2[900:1800],
-    })
-    
-
-def record_why3():
-    st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_3': why_3[:900],
-    })
-    if len(why_3)>900:
-        st.session_state.oocsi.send('LL_qualitative', {
-            'participant_ID': st.session_state.participantID,
-            'why_3_bis': why_3[900:1800],
-        })
-
-def record_why4():
-    st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_4': why_4[:900],
-    })
-    if len(why_4)>900:
-        st.session_state.oocsi.send('LL_qualitative', {
-            'participant_ID': st.session_state.participantID,
-            'why_4_bis': why_4[900:1800],
-        })
-
-def record_why5():
-    st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_5': why_5[:900],
-    })
-    if len(why_5)>900:
-        st.session_state.oocsi.send('LL_qualitative', {
-            'participant_ID': st.session_state.participantID,
-            'why_5_bis': why_5[900:1800],
-        })
-
-def record_why6():
-    st.session_state.oocsi.send('LL_qualitative', {
-        'participant_ID': st.session_state.participantID,
-        'why_6': why_6[:900],
-    })
-    if len(why_6)>900:
-        st.session_state.oocsi.send('LL_qualitative', {
-            'participant_ID': st.session_state.participantID,
-            'why_6_bis': why_6[900:1800],
+            'participant_ID': participant_id,
+            f'why_{question_id}_bis': why[900:1800],
         })
 
 def check_input_length(text):
@@ -105,23 +41,17 @@ def check_input_length(text):
 
 with header2:
     st.title("Comparing the different methods")
-    st.markdown("In this section, we ask you to give you feedback regarding the msummarization tool presented in the experiment.")
+    st.markdown("In this section, we ask you to give you feedback regarding the summarization tool presented in the experiment.")
 
 
 with body2:
     with st.form("my_form"):
-        why = st.text_area(
-                    '**In what situtaions and contexts do you foresee using this summarizing tool?**', "")
-        why_2 = st.text_area(
-            '**How would you typically use the system to assist with reading and understanding text documents?**')
-        why_3 = st.text_area(
-            '**What features would make the system particularly user-firendlu for individuals with Low-literacy levels?**')
-        why_4 = st.text_area(
-        '**What instruction or support might be necessary for new users to learn how to interact woth this system effectively?**')
-        why_5 = st.text_area(
-        '**What oppourtunities do you believe this toolcould create for users with LL?**')
-        why_6 = st.text_area(
-        '**What risk or challenges might arise fro using this system?**')
+        why = st.text_area('**In what situations and contexts do you foresee using this summarizing tool?**', "")
+        why_2 = st.text_area('**How would you typically use the system to assist with reading and understanding text documents?**')
+        why_3 = st.text_area('**What features would make the system particularly user-friendly for individuals with low literacy levels?**')
+        why_4 = st.text_area('**What instruction or support might be necessary for new users to learn how to interact with this system effectively?**')
+        why_5 = st.text_area('**What opportunities do you believe this tool could create for users with low literacy?**')
+        why_6 = st.text_area('**What risks or challenges might arise from using this system?**')
 
         word_count_1 = check_input_length(why)
         word_count_2 = check_input_length(why_2)
@@ -134,32 +64,27 @@ with body2:
 
         if submitted:
             if word_count_1 < 7:
-                st.warning(
-                    'Please explain more extensively your answer (+7 words)')
+                st.warning('Please explain more extensively your answer (+7 words)')
             elif word_count_2 < 7:
-                st.warning(
-                    'Please explain more extensively your answer (+7 words)')
+                st.warning('Please explain more extensively your answer (+7 words)')
             elif word_count_3 < 7:
-                st.warning(
-                    'Please explain more extensively your answer (+7 words)')
+                st.warning('Please explain more extensively your answer (+7 words)')
             elif word_count_4 < 7:
-                st.warning(
-                    'Please explain more extensively your answer (+7 words)')
+                st.warning('Please explain more extensively your answer (+7 words)')
             elif word_count_5 < 7:
-                st.warning(
-                    'Please explain more extensively your answer (+7 words)')
+                st.warning('Please explain more extensively your answer (+7 words)')
             elif word_count_6 < 7:
-                st.warning(
-                    'Please explain more extensively your answer (+7 words)')
+                st.warning('Please explain more extensively your answer (+7 words)')
             else:
                 st.success('Thank you!')
 
+                participant_id = st.session_state.name
 
-                record_why1()
-                record_why2()
-                record_why3()
-                record_why4()
-                record_why5()
-                record_why6()
+                record_why(participant_id, why, 1)
+                record_why(participant_id, why_2, 2)
+                record_why(participant_id, why_3, 3)
+                record_why(participant_id, why_4, 4)
+                record_why(participant_id, why_5, 5)
+                record_why(participant_id, why_6, 6)
 
                 switch_page('thankyou')
